9231f2406ad9dee28dc562a7c37f6f16563c2aa3
william burgos <williamelnene@hotmail.com>
2018-09-10 19:08:05 -0300

    Expresiones regulares

--- Expresiones_regulares/Program.cs
+++ Expresiones_regulares/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Expresiones_regulares
@@ -10,6 +11,12 @@ namespace Expresiones_regulares
     {
         static void Main(string[] args)
         {
+            string patron;
+            patron = "[0-9]+";//Para buscar un valor dado en la cadena de texto.
+            string[] cadena = { "uno1dos2tres3cuatro" }; //elimina lo numero y lo divide en cuatro parte
+            foreach (string texto in cadena)
+                Console.WriteLine(Regex.Replace(texto, patron, "\n"));//Remplaza lo valores numerico en salto de linea;
+            Console.ReadKey();
         }
     }
 }
